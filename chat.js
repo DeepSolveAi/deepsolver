3267d8b895b608fd93b3f4b6c717f884c677d40c
DeepSolveAi <ronakjha407@gmail.com>
2026-03-15 21:47:59 +0530

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const chatController = require("../controllers/chatController");
+const chatController = require("../chatController");
 
 router.post("/", chatController.chat);
 
