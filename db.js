7e95f82c12e61a4cb6c94956ce6e7ce0d1eb3a88
DeepSolveAi <ronakjha407@gmail.com>
2026-03-15 22:44:36 +0530

    Add files via upload

--- db.js
+++ db.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://deepsolver:DeepSolver123@@cluster0.urpfs02.mongodb.net/deepsolver");;
+const MONGO_URI = "mongodb+srv://deepsolver:Deepsolver123@@cluster0.urpfs02.mongodb.net/deepsolver?retryWrites=true&w=majority";
+
+mongoose.connect(MONGO_URI);
 
 const db = mongoose.connection;
 
-db.on("error", console.error.bind(console, "connection error:"));
+db.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
+});
 
 db.once("open", () => {
-  console.log("Database connected");
+  console.log("✅ Database connected successfully");
 });
 
 module.exports = db;
